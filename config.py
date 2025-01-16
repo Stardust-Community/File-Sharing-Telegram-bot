a63e9a7d71b74c3a4a4288dcc2b843869deb28fe
Stardust-Community <arkasarkar235@gmail.com>
2025-01-16 11:34:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -60,10 +60,10 @@ https://dashboard.shareus.io/
 # TRUE for yes FALSE if no 
 USE_SHORTLINK = True if os.environ.get('USE_SHORTLINK', "TRUE") == "TRUE" else False 
 # only shareus service known rightnow rest you can test on your own
-SHORTLINK_API_URL = os.environ.get("SHORTLINK_API_URL", "shortxlinks.com")
+SHORTLINK_API_URL = os.environ.get("SHORTLINK_API_URL", " https://anylinks.in")
 # SHORTLINK_API_KEY = os.environ.get("SHORTLINK_API_KEY", "")
 #use this key if not working ☠️ (jokin!!)
-SHORTLINK_API_KEY = os.environ.get("SHORTLINK_API_KEY", "48e8110960e19f40ceffb1c86c58970c6ea246ef")
+SHORTLINK_API_KEY = os.environ.get("SHORTLINK_API_KEY", "10358845ece3381b197e382342e97766c6c92c14")
 #add your custom time in secs for shortlink expiration.
 # 24hr = 86400
 # 12hr = 43200
